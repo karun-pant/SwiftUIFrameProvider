6d8f0e50f5a0c404a47030ad18f0d6ad15c15c05
karunpant-pcln <karunpant@gmail.com>
2023-02-19 13:30:02 +0530

    Updated Messaging for alert.

--- SwiftUIFrameProvider/SwiftUIFrameProvider/ContentView.swift
+++ SwiftUIFrameProvider/SwiftUIFrameProvider/ContentView.swift
@@ -10,64 +10,75 @@ import SwiftUI
 struct ContentView: View {
     let coordinateSpaceID = "scroll"
     let viewIndexForThreshold: Int
-    let bgColor = [Color.indigo, Color.mint]
-    @State private var bounds: CGRect = .zero
-    @State private var globalFrame: CGRect = .zero
-    @State private var frameInScrollView: CGRect = .zero
+        let bgColor = [Color.indigo, Color.mint]
+        @State private var bounds: CGRect = .zero
+        @State private var globalFrame: CGRect = .zero
+        @State private var frameInScrollView: CGRect = .zero
     @State private var showFrameMessage: Bool = false
     
     var body: some View {
-        ScrollView {
-            VStack (alignment: .leading) {
-                ForEach(0..<30) { i in
-                    Button {
-                        showFrameMessage = true
-                    } label: {
-                        HStack {
-                            Text("Item \(i + 1)")
-                                .foregroundColor(.black)
-                                .padding()
-                            Spacer()
+        VStack {
+            HStack {
+                Text("Stickey Header")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.black)
+                    .padding()
+                Spacer()
+            }
+            .frame(height: 100)
+            .background(Color.gray)
+            ScrollView {
+                VStack (alignment: .leading) {
+                    ForEach(0..<30) { i in
+                        Button {
+                            showFrameMessage = true
+                        } label: {
+                            HStack {
+                                Text("Item \(i + 1)")
+                                    .foregroundColor(.black)
+                                    .padding()
+                                Spacer()
+                            }
+                            .frame(height: 100)
+                            .background(bgColor[ i % 2 ])
                         }
-                        .frame(height: 100)
-                        .background(bgColor[ i % 2 ])
+                        // bounds
+                        .modifier(CoordinateSpaceFrameProvider(
+                            shouldIgnore: viewIndexForThreshold != i,
+                            coordinateSpace: .local,
+                            frame: { frame in
+                                print("Index: \(i) rect: \(frame)")
+                                bounds = frame
+                            }))
+                        // Global frame
+                        .modifier(CoordinateSpaceFrameProvider(
+                            shouldIgnore: viewIndexForThreshold != i,
+                            coordinateSpace: .global,
+                            frame: { frame in
+                                print("Index: \(i) rect: \(frame)")
+                                globalFrame = frame
+                            }))
+                        // frameInScrollView
+                        .modifier(CoordinateSpaceFrameProvider(
+                            shouldIgnore: viewIndexForThreshold != i,
+                            coordinateSpace: .named(coordinateSpaceID),
+                            frame: { frame in
+                                print("Index: \(i) rect: \(frame)")
+                                frameInScrollView = frame
+                            }))
+                        .alert(message,
+                               isPresented: $showFrameMessage,
+                               actions: {
+                            Button("OK", role: .cancel) { }
+                        })
                     }
-                    // bounds
-                    .modifier(CoordinateSpaceFrameProvider(
-                        shouldIgnore: viewIndexForThreshold != i,
-                        coordinateSpace: .local,
-                        frame: { frame in
-                            print("Index: \(i) rect: \(frame)")
-                            bounds = frame
-                        }))
-                    // Global frame
-                    .modifier(CoordinateSpaceFrameProvider(
-                        shouldIgnore: viewIndexForThreshold != i,
-                        coordinateSpace: .global,
-                        frame: { frame in
-                            print("Index: \(i) rect: \(frame)")
-                            globalFrame = frame
-                        }))
-                    // frameInScrollView
-                    .modifier(CoordinateSpaceFrameProvider(
-                        shouldIgnore: viewIndexForThreshold != i,
-                        coordinateSpace: .named(coordinateSpaceID),
-                        frame: { frame in
-                            print("Index: \(i) rect: \(frame)")
-                            frameInScrollView = frame
-                        }))
-                    .alert(message,
-                           isPresented: $showFrameMessage,
-                           actions: {
-                        Button("OK", role: .cancel) { }
-                    })
                 }
             }
         }
     }
     
     private var message: String {
-        let frameMessage = "Global View: \(globalFrame) \n\nView Bounds: \(bounds) \n\nFrame in Perspective to Scroll View or named Coordinate space: \(frameInScrollView)"
+        let frameMessage = "Global View:\n\(globalFrame) \n\nView Bounds:\n\(bounds) \n\nFrame in Perspective to Scroll View or named Coordinate space:\n\(frameInScrollView)"
         return "Tacking View number \(viewIndexForThreshold).\n\n Frame for View number \(viewIndexForThreshold) is:\n\n\(frameMessage)"
     }
 }
